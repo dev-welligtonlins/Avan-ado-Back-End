595b9ac7f4ee004e0e8e1d372ddb12b12e29462e
dev-welligtonlins <welligtonlinsemp@gmail.com>
2022-11-11 16:41:17 -0500

    #8 desafio completo

--- desafio_fetch/js/script.js
+++ desafio_fetch/js/script.js
@@ -1,6 +1,16 @@
 fetch('http://jsonplaceholder.typicode.com/users')
     .then(T => T.json())
     .then((dados => (dados.forEach(item => {
-        console.log(`Nome ${item['id']}`)
-     })))
+        //  console.log(`Nome ${item['name']}, Email ${item['email']}`)
+
+
+            const ul = document.querySelector('.text')
+            // console.log(ul)
+
+            const li = document.createElement('li')
+            li.textContent = `Nome: ${item.name} E-mail: ${item.email}`
+            ul.appendChild(li)
+            console.log(li)
+
+    }))))
 
